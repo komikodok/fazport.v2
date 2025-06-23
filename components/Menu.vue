e591d2462af378556c2e1366151fef0276238515
komikodok <komikodok666@gmail.com>
2025-06-23 21:17:09 +0700

    add Menu

---
+++ components/Menu.vue
@@ -0,0 +1,8 @@
+<template>
+    <div
+        id="map"
+        style="background-image: url('/map.png');"
+        class="cursor-pointer bg-no-repeat bg-cover w-96 h-96 flex justify-center items-center absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 z-10"
+    >
+    </div>
+</template>
--- pages/tes.vue
+++ pages/tes.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import Menu from '~/components/Menu.vue';
 </script>
 
 <template>
-        <div></div>
+        <div class="w-screen h-screen border">
+                <Menu></Menu>
+        </div>
 </template>
 
 <style>
